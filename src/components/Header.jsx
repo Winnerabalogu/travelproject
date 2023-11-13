8ca91477dbf1b589477924a9aa87de34cc19e52c
Winnerabalogu <wabalogu@gmail.com>
2023-11-13 14:14:43 +0100

    last commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import icon from '../images copy/Fill 213.svg'
 
 export default  function Header(){
         return (
             <nav className='navbar'>
-                    <h1>travelvloggers</h1>
+                    <h1><img src={icon} className='icon-logo'/>travelvloggers</h1>
             </nav>
         );
   
